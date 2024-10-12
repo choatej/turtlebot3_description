d3af76a632ac5194efa2b4cd3e825db66bbfce71
Jon Choate <choatej@gmail.com>
2024-10-12 16:37:09 -0400

    fix: adding coveragepy-result

--- launch/turtlebot3_burger.launch.py
+++ launch/turtlebot3_burger.launch.py
@@ -14,50 +14,52 @@ import xacro
 
 log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
 logging.basicConfig(level=log_level)
-logging.info("logging configured")
+logging.info('logging configured')
 
-robot_name = "turtlebot3"
-package_name = "turtlebot3_description"
+robot_name = 'turtlebot3'
+package_name = 'turtlebot3_description'
 base_xacro_file = 'turtlebot3_burger.urdf.xacro'
 install_dir = get_package_prefix(package_name)
 robot_model_path = os.path.join(get_package_share_directory(package_name))
 xacro_file = os.path.join(robot_model_path, 'urdf', base_xacro_file)
-x = "0.0"
-y = "0.0"
-z = "0.0"
+x = '0.0'
+y = '0.0'
+z = '0.0'
+
 
 def process_xacro_file(xacro_file: PathLike) -> str:
     # convert XACRO file into URDF
-    logging.debug("parsing xacro doc")
+    logging.debug('parsing xacro doc')
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
-    return doc.toprettyxml(indent="    ")
+    return doc.toprettyxml(indent='    ')
+
 
 def set_gazebo_env_vars():
     gazebo_models_path = os.path.join(package_name, 'urdf')
     if 'GAZEBO_MODEL_PATH' in os.environ:
         os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + \
-            ':' + install_dir + '/share' + ':' + gazebo_models_path
+                                          ':' + install_dir + '/share' + ':' + gazebo_models_path
     else:
         os.environ['GAZEBO_MODEL_PATH'] = install_dir + \
-            "/share" + ':' + gazebo_models_path
+                                          '/share' + ':' + gazebo_models_path
 
     if 'GAZEBO_PLUGIN_PATH' in os.environ:
         os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + \
-            ':' + install_dir + '/lib'
+                                           ':' + install_dir + '/lib'
     else:
         os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
 
-    logging.debug("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
-    logging.debug("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+    logging.debug('GAZEBO MODELS PATH==' + str(os.environ['GAZEBO_MODEL_PATH']))
+    logging.debug('GAZEBO PLUGINS PATH==' + str(os.environ['GAZEBO_PLUGIN_PATH']))
 
 
 def generate_robot_state_publisher_node():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     urdf_text = process_xacro_file(xacro_file)
     # save it for debugging
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    outfile_file = os.path.join("/tmp", f"{os.path.splitext(os.path.split(xacro_file)[-1])[0]}-{timestamp}.urdf")
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    outfile_file = os.path.join('/tmp', f'{os.path.splitext(os.path.split(xacro_file)[-1])[0]}-{timestamp}.urdf')
     with open(outfile_file, 'w') as f:
         f.write(urdf_text)
 
@@ -71,17 +73,17 @@ def generate_robot_state_publisher_node():
 
 
 def generate_robot_spawn():
-    logging.debug(f'robot spawn topic is robot_description')
+    logging.debug('robot spawn topic is robot_description')
     return Node(
-    name='spawn_entity',
-    package='gazebo_ros',
-    executable='spawn_entity.py',
-    arguments=['-entity', robot_name, '-x', x, '-y', y, '-z', z, '-topic', 'robot_description']
-)
+        name='spawn_entity',
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', robot_name, '-x', x, '-y', y, '-z', z, '-topic', 'robot_description']
+    )
 
 
 def genrate_static_transform_publisher_node(world_frame):
-    logging.debug(f'creating static transform')
+    logging.debug('creating static transform')
     return Node(
         package='tf2_ros',
         executable='static_transform_publisher',
@@ -90,6 +92,7 @@ def genrate_static_transform_publisher_node(world_frame):
         emulate_tty=True,
         arguments=[x, y, z, '0', '0', '0', world_frame, 'base_link']
     )
+
 
 def generate_launch_description():
     logging.debug('starting generate_launch_description')
@@ -109,33 +112,33 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 [os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-                launch_arguments={
-                    'verbose': 'false',
-                    'pause': 'false',
-                    'world': LaunchConfiguration('world'),
-                    }.items(),
+            launch_arguments={
+                'verbose': 'false',
+                'pause': 'false',
+                'world': LaunchConfiguration('world'),
+            }.items(),
         ),
     ]
-    logging.debug("created base launch config, now appending stuff")
+    logging.debug('created base launch config, now appending stuff')
     delayed_start = TimerAction(
-        actions = [],
-        period = 5.0
+        actions=[],
+        period=5.0
     )
     launch_description_items.append(LogInfo(msg='Gazebo started. Launching robot nodes.'))
     launch_description_items.append(delayed_start)
-    logging.debug("appended delay start")
+    logging.debug('appended delay start')
     # start the robots after waiting a bit for gazebo
     logging.debug(f'defining {robot_name}')
     state_publisher = generate_robot_state_publisher_node()
-    logging.debug("state publisher added")
+    logging.debug('state publisher added')
     delayed_start.actions.append(state_publisher)
-    logging.debug("delayed start added")
+    logging.debug('delayed start added')
     robot_spawn = generate_robot_spawn()
-    logging.debug("robot spawn created")
+    logging.debug('robot spawn created')
     delayed_start.actions.append(robot_spawn)
-    logging.debug("robot spawn added")
+    logging.debug('robot spawn added')
     static_transform = genrate_static_transform_publisher_node(world_frame)
-    logging.debug("static transform created")
+    logging.debug('static transform created')
     delayed_start.actions.append(static_transform)
-    logging.debug("static transform added")
+    logging.debug('static transform added')
     return LaunchDescription(launch_description_items)
